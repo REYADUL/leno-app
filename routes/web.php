e851684044b4a6bbf31e16488bdef383222b0449
Reyadul Islam <reyadul.islam792@gmail.com>
2023-06-26 19:35:56 +0600

    html to blade

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/terms-condition', function () {
+    return view('terms-conditions');
+});
+Route::get('/privacy-policy', function () {
+    return view('privacy-policy');
+});
